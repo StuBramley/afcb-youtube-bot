4111ff7e894030df5002e8e5774cd0a5aef32c43
Stuart Bramley <stu.bramley@kfa.co.uk>
2025-01-06 23:38:57 +0000

    Compile!

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -38,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.notifier = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const api_1 = __importStar(require("@atproto/api"));
+const api_1 = __importDefault(require("@atproto/api"));
 dotenv_1.default.config();
 var session = null;
 const YouTubeNotifier = require('youtube-notification');
@@ -96,16 +73,11 @@ function postToBlueSky(videoUrl, createdAt) {
             console.log('Refreshing session');
             yield agent.resumeSession(session);
         }
-        const rt = new api_1.RichText({
-            text: videoUrl
-        });
         var postRecord = {
             $type: 'app.bsky.feed.post',
-            text: rt.text,
-            facets: rt.facets,
+            text: videoUrl,
             createdAt: createdAt
         };
-        yield rt.detectFacets(agent);
         console.log('agent posting ' + videoUrl);
         yield agent.post(postRecord);
     });
